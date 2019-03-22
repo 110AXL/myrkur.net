3dd22fd2e292b34eabeca28e909e10292cf655c5
zc4r <ekkert@myrkur.net>
2019-03-22 16:27:44 +0000

    welcome and login

--- medlimir/login.php
+++ medlimir/login.php
@@ -7,10 +7,10 @@
     <style type='text/css'>
       body{ font: 14px sans-serif; }
       .wrapper{ width: 350px; padding: 20px; }
-    </style>";
+    </style>
 <?php    include('../main/facebookPixelCode.php'); ?>
   </head>
-  <body bgcolor='black'>
+  <body>
     <div class='wrapper'>
       <h2>Login</h2>
       <p>Please fill in your credentials to login.</p>
@@ -18,17 +18,16 @@
         <div class='form-group <?php echo (!empty($username_err)) ? 'has-error' : '';?>'>
           <label>Username</label>
           <input type='text' name='username' class='form-control' value='<?php echo $username; ?> '>
-          <span class='help-block'><?php echo $username_err; ?>"</span>
+          <span class='help-block'><?php echo $username_err; ?></span>
         </div>
         <div class='form-group "<?php echo (!empty($password_err)) ? 'has-error' : ''; ?> "'>
           <label>Password</label>
           <input type='password' name='password' class='form-control'>
-          <span class='help-block'><?php echo $password_err; ?> "</span>
+          <span class='help-block'><?php echo $password_err; ?> </span>
         </div>
         <div class='form-group'>
           <input type='submit' class='btn btn-primary' value='Login'>
         </div>
         <p>Don't have an account? <a href='register.php'>Sign up now</a>.</p>
       </form>
-    </div>";
-?>
+    </div>
