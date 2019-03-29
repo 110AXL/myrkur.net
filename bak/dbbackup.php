dc51ef749086601e505bb52d2bd9abc521053604
zc4r <ekkert@myrkur.net>
2019-03-29 16:27:59 +0000

    db

--- bak/dbbackup.php
+++ bak/dbbackup.php
@@ -1,14 +1,9 @@
 <?php
-$username = “u445255185_zc4r”;
-$password = “Dim&mur%Dalur”;
-$hostname = “localhost”;
-$database = “u445255185_sweet”;
-$username =escapeshellcmd($username);
-$password =escapeshellcmd($password);
-$hostname =escapeshellcmd($hostname);
-$database =escapeshellcmd($database);
-$backupFile=’/home/u445255185/domains/myrkur.net/public_html/bak/’.date(“Y-m-d-H-i-s”).$database.’.sql’;
-$command = “mysqldump -u".$username."-p".$password."-h".$hostname $database > $backupFile”;
-system($command, $result);
-echo $result;
+$DATABASE="u445255185_sweet";
+$DBUSER="u445255185_zc4r";
+$DBPASSWD="Dim&mur%Dalur";
+$PATH="/home/u445255185/domains/myrkur.net/public_html/bak/";
+$FILE_NAME="myrkur.net-backup-" . date("Y-m-d") . ".sql.gz";
+exec('/usr/bin/mysqldump -u '.$DBUSER.' -p'.$DBPASSWD.' '.$DATABASE.' | gzip --best > '.$PATH.$FILE_NAME);
+echo "Database(".$DATABASE.") backup completed. File name: ".$FILE_NAME;
 ?>
