e178238f0500b332f68a67aefffe96648ca33584
zc4r <ekkert@myrkur.net>
2019-03-29 12:16:06 +0000

    upl

--- medlimir/upload.php
+++ medlimir/upload.php
@@ -18,7 +18,7 @@ session_start();
 if(!isset($_SESSION['username'])){
    header("Location:index.php");
 }
-include '../hidden/sqlcon.php';
+
 include '../res/ipgrabber.php';
 echo "<center>";
 include '../res/logo.php';
@@ -88,6 +88,7 @@ if (strpos($link2, 'https') !== false) {
 	 echo "<p>http added to link 2.</p>";
 }
 
+require_once '../hidden/sqlcon.php';
 /* Prepared statement, stage 1: prepare */
 if (!($stmt = $mysqli->prepare("INSERT INTO albums(artist, album, year, genre, link1, link2, img, user_id) VALUES (?,?,?,?,?,?,?,?)"))) {
 	echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
