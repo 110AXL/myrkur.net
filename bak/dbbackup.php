30f3010693bbc9eee1a8b9115ce6e6b765e01ffd
zc4r <ekkert@myrkur.net>
2019-03-29 16:22:25 +0000

    db

--- bak/dbbackup.php
+++ bak/dbbackup.php
@@ -1,13 +1,13 @@
 <?php
-$username = “MySQL database username”;
-$password = “MySQL database password”;
-$hostname = “MySQL database hostname”;
-$database = “Name of your MySQL database”;
+$username = “u445255185_zc4r”;
+$password = “Dim&mur%Dalur”;
+$hostname = “localhost”;
+$database = “u445255185_sweet”;
 $username =escapeshellcmd($username);
 $password =escapeshellcmd($password);
 $hostname =escapeshellcmd($hostname);
 $database =escapeshellcmd($database);
-$backupFile=’/home/www/example.com/backuprestricted/’.date(“Y-m-d-H-i-s”).$database.’.sql’;
+$backupFile=’/home/u445255185/domains/myrkur.net/public_html/bak/’.date(“Y-m-d-H-i-s”).$database.’.sql’;
 $command = “mysqldump -u$username -p$password -h$hostname $database > $backupFile”;
 system($command, $result);
 echo $result;
