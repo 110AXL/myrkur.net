01829fa6526f544702291d2240fe98e2aaca6b22
zc4r <ekkert@myrkur.net>
2019-03-29 16:50:43 +0000

    -box

--- index.php
+++ index.php
@@ -13,7 +13,10 @@
 <body bgcolor="black">
  <?php
 session_start();
-require_once 'main/sqlcon.php';
+$mysqli = new mysqli("localhost", "u445255185_zc4r", "Dim&mur%Dalur", "u445255185_sweet");
+if ($mysqli->connect_errno) {
+	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
 include 'medlimir/res/ip.php';
 include 'main/header.php';
 include 'medlimir/res/logo.php';
