8e5177e9696aac9a336e58e92de76e28343c31fa
zc4r <ekkert@myrkur.net>
2019-03-29 15:14:12 +0000

    sortering

--- index.php
+++ index.php
@@ -7,16 +7,16 @@
 <head>
 <link rel="stylesheet" type="text/css" href="res/default.css">
 
-<?php include('res/facebookPixelCode.php'); ?>
+<?php include('medlimir/res/facebookPixelCode.php'); ?>
 <link rel="shortcut icon" type="image/png" href="main/favicon-16x16.png"/>
 </head>
 <body bgcolor="black">
  <?php
 session_start();
 require_once 'medlimir/res/sqlcon.php';
-include 'res/ip.php';
+include 'medlimir/res/ip.php';
 include 'main/header.php';
-include 'res/logo.php';
+include 'medlimir/res/logo.php';
 echo "<div id='P'>";
     include 'main/box.php';
 echo "</div>";
