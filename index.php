e9a6d82cc793554ce52ebf105f68642397629a68
zc4r <ekkert@myrkur.net>
2019-03-29 15:53:43 +0000

    -box

--- index.php
+++ index.php
@@ -18,7 +18,33 @@ include 'medlimir/res/ip.php';
 include 'main/header.php';
 include 'medlimir/res/logo.php';
 echo "<div id='P'>";
-    include 'box.php';
+
+if (!empty($_GET["genre"])) {
+	echo "<h1>Genre: " . $_GET["genre"] . "</h1>";
+	$genre = $_GET["genre"];
+    $sql = "SELECT albums.id, albums.artist, albums.album, albums.link1, albums.img, users.username FROM albums LEFT JOIN users ON albums.user_id = users.id WHERE albums.genre ='". $genre ."' ORDER BY albums.id DESC";
+	}
+	else {
+    $sql = "SELECT albums.id, albums.artist, albums.album, albums.link1, albums.img, users.username FROM albums LEFT JOIN users ON albums.user_id = users.id ORDER BY albums.id DESC";
+	}
+
+
+$result = $mysqli->query($sql);
+echo "<div class='grid-container grid-container--fill'>";
+
+
+if ($result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		$resized_link = $row["img"];
+		$removed_spaces = str_replace(" ","%20",$resized_link);
+		echo "<div class='grid-element'><a target=_blank title='" . $row["artist"]. " - " . $row["album"]. " [" . $row["username"] . "]' href=main/link.php?i='" . $row["id"]."'><img width=200 height=200 src=medlimir/uploads/" . $removed_spaces . " /><div class='title'><p>" . $row["artist"] . " - " . $row["album"] . "</a></div></p></div>";
+		}
+	}
+
+$mysqli->close();
+echo "</div>";
+
 echo "</div>";
 echo "Thanks, man.";
 ?>
