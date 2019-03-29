5571aaf77e587cf8b673a198a0adaf75e79ab48d
zc4r <ekkert@myrkur.net>
2019-03-29 18:08:52 +0000

    css

--- medlimir/welcome.php
+++ medlimir/welcome.php
@@ -14,7 +14,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <head>
     <meta charset="UTF-8">
     <title>Welcome</title>
-    <link rel="stylesheet" href="res/default.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <style type="text/css">
         body{ font: 14px sans-serif; text-align: center; }
     </style>
@@ -38,15 +38,27 @@ $sql = "SELECT username, email, twitter, twitch FROM users ORDER BY username, tw
 $result = $mysqli->query($sql);
 
 if ($result->num_rows > 0) {
-    // output data of each row
+    // output data of each row<div class="w3-container">
+  echo "<h2>Userlist</h2>
+
+  <table class='w3-table-all w3-hoverable'>
+    <thead>
+      <tr class='w3-light-grey'>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Twitter & Twitch</th>
+      </tr>
+    </thead>";
     while($row = $result->fetch_assoc()) {
-        echo "<div class='col-sm'><ul class=”userList”><li><a href=/medlimir/?nafn=" . $row["username"]. "><h2>" . $row["username"] . "</h2></a> <a href=mailto:" . $row["email"] . "><h3>" . $row["email"] . "</h3></a></li>";
+        echo "<tr><td><a href=/medlimir/?nafn=" . $row["username"]. ">" . $row["username"] . "</a>";
+        echo "<td><a href=mailto:" . $row["email"] . ">" . $row["email"] . "</a></td>";
      if(!empty($row["twitter"]))
-       echo "<li class='twitterIcon'><h4><a href=http://twitter.com/" . $row["twitter"] . "><img src='res/Twitter_Logo_Blue.png' class='icon' /> " . $row["twitter"] . "</a></h4></li>";
-     echo"</div>";
-     if(!empty($row["twitch"]))
-       echo "<li class='twitchIcon'><h4><a href=http://twitch.tv/" . $row["twitch"] . "><img src='res/Combo_Purple_RGB.png' class='icon' /> " . $row["twitch"] . "</a></h4></li>";
-     echo"</ul></div>";
+       echo "<td class='twitterIcon'><a href=http://twitter.com/" . $row["twitter"] . "><img src='res/Twitter_Logo_Blue.png' class='icon' /></a>";
+     if(!empty($row["twitch"])){
+       echo " ";
+       echo "<td class='twitchIcon'><a href=http://twitch.tv/" . $row["twitch"] . "><img src='res/Glitch_Purple_RGB.png' class='icon' /></a></td>";
+     }
+     echo "<tr></div>";
     }
 } else {
     echo "0 results";
