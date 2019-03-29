8943d7efce8a7f4f8934c365873db22375d97132
zc4r <ekkert@myrkur.net>
2019-03-29 06:46:51 +0000

    tv

--- tv/index.php
+++ tv/index.php
@@ -11,6 +11,8 @@
 <?php    include('../main/facebookPixelCode.php'); ?>
 </head>
 <body>
-<iframe src="https://player.twitch.tv/?channel=110axl" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe><a href="https://www.twitch.tv/110axl?tt_content=text_link&tt_medium=live_embed" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;">Watch live video from 110axl on www.twitch.tv</a>
+  <p>
+    <iframe src="https://player.twitch.tv/?channel=110axl" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe><a href="https://www.twitch.tv/110axl?tt_content=text_link&tt_medium=live_embed" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;">Watch live video from 110axl on www.twitch.tv</a>
+  </p>
 </body>
 </html>
