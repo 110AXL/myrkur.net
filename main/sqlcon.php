94153a09474b6de65b7c38fc4f288a187606f78d
zc4r <ekkert@myrkur.net>
2019-03-29 17:04:17 +0000

    sql

--- main/sqlcon.php
+++ main/sqlcon.php
@@ -2,5 +2,6 @@
 $mysqli = new mysqli("localhost", "u445255185_zc4r", "Dim&mur%Dalur", "u445255185_sweet");
 if ($mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
+} else
+	echo "Connected to database."
 ?>
