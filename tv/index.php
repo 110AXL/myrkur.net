135bbdcc2536c524b5476d51a2e916887b032e90
zc4r <ekkert@myrkur.net>
2019-03-29 06:33:55 +0000

    tv

--- tv/index.php
+++ tv/index.php
@@ -2,7 +2,7 @@
 <html lang='en'>
 <head>
   <meta charset='UTF-8'>
-  <title>Login</title>
+  <title>TV</title>
   <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css'>
   <style type='text/css'>
     body{ font: 14px sans-serif; }
@@ -12,3 +12,5 @@
 </head>
 <body>
 <iframe src="https://player.twitch.tv/?channel=110axl" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe><a href="https://www.twitch.tv/110axl?tt_content=text_link&tt_medium=live_embed" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;">Watch live video from 110axl on www.twitch.tv</a>
+</body>
+</html>
