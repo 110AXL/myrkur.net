bea2de81db2a0c16b5d3348a5728b734bce62829
zc4r <ekkert@myrkur.net>
2019-03-29 12:35:35 +0000

    Update sqlcon.php
    sql

--- hidden/sqlcon.php
+++ hidden/sqlcon.php
@@ -1,4 +1,5 @@
 <?php
+echo "sqlcon.php loaded";
 $mysqli = new mysqli("localhost", "u445255185_zc4r", "AXL110axl", "u445255185_sweet");
 if ($mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
