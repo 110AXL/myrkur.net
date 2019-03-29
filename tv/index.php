2710a5d46b6bad559cfd2493060cc4c20f6681b8
zc4r <ekkert@myrkur.net>
2019-03-29 06:14:42 +0000

    TV updates

---
+++ tv/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+  <meta charset='UTF-8'>
+  <title>Login</title>
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css'>
+  <style type='text/css'>
+    body{ font: 14px sans-serif; }
+    .wrapper{ width: 350px; padding: 20px; }
+  </style>
+<?php    include('../main/facebookPixelCode.php'); ?>
+</head>
+<body>
+<iframe src="https://player.twitch.tv/?channel=110axl" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe><a href="https://www.twitch.tv/110axl?tt_content=text_link&tt_medium=live_embed" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;">Watch live video from 110axl on www.twitch.tv</a>
