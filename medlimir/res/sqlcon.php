e264bdb505bb3af845313d9fa064f84118b83a46
zc4r <ekkert@myrkur.net>
2019-03-29 21:08:47 +0000

    sql

--- medlimir/res/sqlcon.php
+++ medlimir/res/sqlcon.php
@@ -4,4 +4,5 @@ if ($mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 } else
 	$sqlcon = "Tengdur gagnagrunni.";
+	echo get_include_path();
 ?>
