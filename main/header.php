df70de2558cfdf2e0d86aceaca7b67a442dd6cfb
zc4r <ekkert@myrkur.net>
2019-04-15 12:07:57 +0000

    s

--- main/header.php
+++ main/header.php
@@ -1,28 +1,41 @@
-<div class="header">
-	<div class="add"><a href='https://www.myrkur.net/medlimir/addalbum.php'>
-		<button class="addbtn"><img class="left_centered" src="https://www.myrkur.net/main/add.png" width="9" height="9"/>&nbsp;&nbsp;&nbsp;Add</button></a>
-		 <?php echo $sqlcon; ?>
-		</div>
-		<div class="center">myrkur.net</div>
-		<div class="genre">
-	  <button class="genrebtn"><img class="right_centered" src="https://www.myrkur.net/main/down.png" width="9" height="9"/>&nbsp;&nbsp;&nbsp;Genre</button>
-	  <div class="genre-content">
-	    <a href="http://www.myrkur.net/?genre=ambient">Ambient</a>
-		<a href="http://www.myrkur.net/?genre=country">Country</a>
-		<a href="http://www.myrkur.net/?genre=hiphop">Hiphop</a>
-		<a href="http://www.myrkur.net/?genre=idk">I don't know</a>
-	    <a href="http://www.myrkur.net/?genre=metal">Metal</a>
-		<a href="http://www.myrkur.net/?genre=pop">Pop</a>
-		<a href="http://www.myrkur.net/?genre=rock">Rock</a>
-		</div></div>
-	<div class="dropdown">
-	  <button class="dropbtn">Menu&nbsp;&nbsp;&nbsp;<img class="right_centered" src="https://www.myrkur.net/main/down.png" width="9" height="9"/></button>
-	  <div class="dropdown-content">
-	    <a target=_blank href="http://members.myrkur.net/">Login</a>
-	    <a target=_blank href="http://members.myrkur.net/register.php">Register</a>
-		<a target=_blank href="http://members.myrkur.net/movies/">Movies</a>
-		<a target=_blank href="http://www.myrkur.net">Music</a>
-		<a target=_blank href="http://myrkur.net/tv/">TV</a>
-	  </div>
+<div class="grid">
+	<div class="header">
+		<aside class="sidebar-left">
+			<div class="add"><a href='https://www.myrkur.net/medlimir/addalbum.php'>
+				<button class="addbtn"><img class="left_centered" src="https://www.myrkur.net/main/add.png" width="9" height="9"/>&nbsp;&nbsp;&nbsp;Add</button></a>
+				 <?php echo $sqlcon; ?>
+				</div>
+				<div class="center">myrkur.net</div>
+				<div class="genre">
+			  <button class="genrebtn"><img class="right_centered" src="https://www.myrkur.net/main/down.png" width="9" height="9"/>&nbsp;&nbsp;&nbsp;Genre</button>
+			  <div class="genre-content">
+			    <a href="http://www.myrkur.net/?genre=ambient">Ambient</a>
+				<a href="http://www.myrkur.net/?genre=country">Country</a>
+				<a href="http://www.myrkur.net/?genre=hiphop">Hiphop</a>
+				<a href="http://www.myrkur.net/?genre=idk">I don't know</a>
+			    <a href="http://www.myrkur.net/?genre=metal">Metal</a>
+				<a href="http://www.myrkur.net/?genre=pop">Pop</a>
+				<a href="http://www.myrkur.net/?genre=rock">Rock</a>
+				</div></div>
+		</aside>
+			<article>
+				Myrkur.net
+			</article>
+		<aside class="sidebar-right">
+			<div class="dropdown">
+			  <button class="dropbtn">Menu&nbsp;&nbsp;&nbsp;<img class="right_centered" src="https://www.myrkur.net/main/down.png" width="9" height="9"/></button>
+			  <div class="dropdown-content">
+			    <a target=_blank href="http://members.myrkur.net/">Login</a>
+			    <a target=_blank href="http://members.myrkur.net/register.php">Register</a>
+					<a target=_blank href="http://members.myrkur.net/movies/">Movies</a>
+					<a target=_blank href="http://www.myrkur.net">Music</a>
+					<a target=_blank href="http://myrkur.net/tv/">TV</a>
+			  </div>
+			</div>
+		</aside>
 	</div>
+
+  <footer>
+    Footer
+  </footer>
 </div>
