caf16be9e565b7ed1517852536f2a6c0e586fe76
zc4r <ekkert@myrkur.net>
2019-04-16 01:12:55 +0000

    s

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // Initialize the session
 session_start();
-
+/*
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     $loggedIn = 0;
@@ -9,7 +9,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 } if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
    $loggedIn = 1;
    echo "logged in.<br/>";
-}
+}*/
 ?>
 <!DOCTYPE html>
 
