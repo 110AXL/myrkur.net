8f3bb774a434ca39af699f2012c4aac91cb9ff45
zc4r <ekkert@myrkur.net>
2019-04-16 01:00:48 +0000

    s

--- medlimir/socialupdate.php
+++ medlimir/socialupdate.php
@@ -10,245 +10,263 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
 $id = $_SESSION['id'];
 
-// Check if the hash of the password field matches the user's password hash, if not then redirect him to Social page
-if(!isset($_POST["password"])){
-    header("location: social.php");
-    exit;
-}
-
 // Define variables and initialize with empty values
 $twitter = $youtube = $twitch = $psn = $email = $discord = $passi= "";
 
 
-$pash = password_hash($_POST["password"], PASSWORD_DEFAULT);
-echo $pash . " <- pash <br/>";
+if(empty(trim($_POST["password"]))){
+		$password_err = "Please enter your password.";
+} else{
+		$password = trim($_POST["password"]);
+}
 
-if (isset($pash))
-{
-	//connect to db
-	include "sqlcon.php";
-	$sql = "SELECT password FROM users WHERE id='$id'";
-	$result = $mysqli->query($sql);
+if(empty($password_err)){
+			include "sqlcon.php";
+			// Prepare a select statement
+			$sql = "SELECT id, password FROM users WHERE id = ?";
 
-	if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $passi = $row["password"];
-    }
-		} else {
-		    echo "0 results";
-		}
-	echo $sql . " <- sql<br/>";
-	echo $passi . " <- result";
+			if($state = mysqli_prepare($mysqli, $sql)){
+					// Bind variables to the prepared statement as parameters
+					mysqli_stmt_bind_param($state, "s", $param_id);
 
-	// if hash provided matches database hash continue
-	if($pash==$result)
-	{
+					// Set parameters
+					$param_id = $id;
 
-		/* Check if POST contains twitch url */
-		if(isset($_POST['twitch']))
-		{
-			$twitch = $_POST['twitch'];
-			echo "<p>Twitch account set to: ". $twitch ."</p>";
+					// Attempt to execute the prepared statement
+					if(mysqli_stmt_execute($state)){
+							// Store result
+							mysqli_stmt_store_result($state);
 
-		  $stmt = $mysqli->prepare("UPDATE users SET twitch=? WHERE id=?");
-		  /* BK: always check whether the prepare() succeeded */
-		  if ($stmt === false) {
-		    trigger_error($mysqli->error, E_USER_ERROR);
-		    return;
-		  }
-		  /* Bind our params */
-		  /* BK: variables must be bound in the same order as the params in your SQL.
-		   * Some people prefer PDO because it supports named parameter. */
-		  $stmt->bind_param('si', $twitch, $id);
+							// Check if username exists, if yes then verify password
+							if(mysqli_stmt_num_rows($state) == 1){
+									// Bind result variables
+									mysqli_stmt_bind_result($state, $id, $hashed_password);
+									if(mysqli_stmt_fetch($state)){
+											if(password_verify($password, $hashed_password)){
+													// Password is correct, update the database
 
-		  /* Set our params */
-		  /* BK: No need to use escaping when using parameters, in fact, you must not,
-		   * because you'll get literal '\' characters in your content. */
-		/*  $twitch = $_POST['twitch'] ?: '';*/
+													{
 
-		  /* Execute the prepared Statement */
-		  $status = $stmt->execute();
-		  /* BK: always check whether the execute() succeeded */
-		  if ($status === false) {
-		    trigger_error($stmt->error, E_USER_ERROR);
-		  }
-		  printf("<p>Twitch %d Row inserted.</p>\n", $stmt->affected_rows);
-		}
+														/* Check if POST contains twitch url */
+														if(isset($_POST['twitch']))
+														{
+															$twitch = $_POST['twitch'];
+															echo "<p>Twitch account set to: ". $twitch ."</p>";
 
-		/* Check if POST contains discord username */
-		if(isset($_POST['discord']))
-		{
-			$twitch = $_POST['discord'];
-			echo "<p>Discord account set to: ". $discord ."</p>";
+														  $stmt = $mysqli->prepare("UPDATE users SET twitch=? WHERE id=?");
+														  /* BK: always check whether the prepare() succeeded */
+														  if ($stmt === false) {
+														    trigger_error($mysqli->error, E_USER_ERROR);
+														    return;
+														  }
+														  /* Bind our params */
+														  /* BK: variables must be bound in the same order as the params in your SQL.
+														   * Some people prefer PDO because it supports named parameter. */
+														  $stmt->bind_param('si', $twitch, $id);
 
-		  $stmt = $mysqli->prepare("UPDATE users SET discord=? WHERE id=?");
-		  /* BK: always check whether the prepare() succeeded */
-		  if ($stmt === false) {
-		    trigger_error($mysqli->error, E_USER_ERROR);
-		    return;
-		  }
-		  /* Bind our params */
-		  /* BK: variables must be bound in the same order as the params in your SQL.
-		   * Some people prefer PDO because it supports named parameter. */
-		  $stmt->bind_param('si', $discord, $id);
+														  /* Set our params */
+														  /* BK: No need to use escaping when using parameters, in fact, you must not,
+														   * because you'll get literal '\' characters in your content. */
+														/*  $twitch = $_POST['twitch'] ?: '';*/
 
-		  /* Set our params */
-		  /* BK: No need to use escaping when using parameters, in fact, you must not,
-		   * because you'll get literal '\' characters in your content. */
-		/*  $twitch = $_POST['twitch'] ?: '';*/
+														  /* Execute the prepared Statement */
+														  $status = $stmt->execute();
+														  /* BK: always check whether the execute() succeeded */
+														  if ($status === false) {
+														    trigger_error($stmt->error, E_USER_ERROR);
+														  }
+														  printf("<p>Twitch %d Row inserted.</p>\n", $stmt->affected_rows);
+														}
 
-		  /* Execute the prepared Statement */
-		  $status = $stmt->execute();
-		  /* BK: always check whether the execute() succeeded */
-		  if ($status === false) {
-		    trigger_error($stmt->error, E_USER_ERROR);
-		  }
-		  printf("<p>Discord %d Row inserted.</p>\n", $stmt->affected_rows);
-		}
+														/* Check if POST contains discord username */
+														if(isset($_POST['discord']))
+														{
+															$twitch = $_POST['discord'];
+															echo "<p>Discord account set to: ". $discord ."</p>";
 
-		/* Check if POST contains Playstation network username */
-		if(isset($_POST['psn']))
-		{
-			$psn = $_POST['psn'];
-			echo "<p>Playstation Network account set to: ". $psn ."</p>";
+														  $stmt = $mysqli->prepare("UPDATE users SET discord=? WHERE id=?");
+														  /* BK: always check whether the prepare() succeeded */
+														  if ($stmt === false) {
+														    trigger_error($mysqli->error, E_USER_ERROR);
+														    return;
+														  }
+														  /* Bind our params */
+														  /* BK: variables must be bound in the same order as the params in your SQL.
+														   * Some people prefer PDO because it supports named parameter. */
+														  $stmt->bind_param('si', $discord, $id);
 
-		  $stmt = $mysqli->prepare("UPDATE users SET psn=? WHERE id=?");
-		  /* BK: always check whether the prepare() succeeded */
-		  if ($stmt === false) {
-		    trigger_error($mysqli->error, E_USER_ERROR);
-		    return;
-		  }
-		  /* Bind our params */
-		  /* BK: variables must be bound in the same order as the params in your SQL.
-		   * Some people prefer PDO because it supports named parameter. */
-		  $stmt->bind_param('si', $psn, $id);
+														  /* Set our params */
+														  /* BK: No need to use escaping when using parameters, in fact, you must not,
+														   * because you'll get literal '\' characters in your content. */
+														/*  $twitch = $_POST['twitch'] ?: '';*/
 
-		  /* Set our params */
-		  /* BK: No need to use escaping when using parameters, in fact, you must not,
-		   * because you'll get literal '\' characters in your content. */
-		/*  $twitch = $_POST['twitch'] ?: '';*/
+														  /* Execute the prepared Statement */
+														  $status = $stmt->execute();
+														  /* BK: always check whether the execute() succeeded */
+														  if ($status === false) {
+														    trigger_error($stmt->error, E_USER_ERROR);
+														  }
+														  printf("<p>Discord %d Row inserted.</p>\n", $stmt->affected_rows);
+														}
 
-		  /* Execute the prepared Statement */
-		  $status = $stmt->execute();
-		  /* BK: always check whether the execute() succeeded */
-		  if ($status === false) {
-		    trigger_error($stmt->error, E_USER_ERROR);
-		  }
-		  printf("<p>Playstation Network %d Row inserted.</p>\n", $stmt->affected_rows);
-		}
+														/* Check if POST contains Playstation network username */
+														if(isset($_POST['psn']))
+														{
+															$psn = $_POST['psn'];
+															echo "<p>Playstation Network account set to: ". $psn ."</p>";
 
-		/* Check if POST contains Youtube channel */
-		if(isset($_POST['youtube']))
-		{
+														  $stmt = $mysqli->prepare("UPDATE users SET psn=? WHERE id=?");
+														  /* BK: always check whether the prepare() succeeded */
+														  if ($stmt === false) {
+														    trigger_error($mysqli->error, E_USER_ERROR);
+														    return;
+														  }
+														  /* Bind our params */
+														  /* BK: variables must be bound in the same order as the params in your SQL.
+														   * Some people prefer PDO because it supports named parameter. */
+														  $stmt->bind_param('si', $psn, $id);
 
-			if(substr(trim($_POST["youtube"]), 0, 23) == "https://www.youtube.com"){
-	        $youtube = trim($_POST["youtube"]);
-	    } else if (substr(trim($_POST["youtube"]), 0, 22) == "http://www.youtube.com")
-	      {
-	        $youtube = trim($_POST["youtube"]);
-	    } else if (substr(trim($_POST["youtube"]), 0, 19) == "https://youtube.com")
-	      {
-	        $youtube = trim($_POST["youtube"]);
-	    } else if (substr(trim($_POST["youtube"]), 0, 18) == "http://youtube.com")
-	      {
-	        $youtube = trim($_POST["youtube"]);
-	    } else {
-	        $youtube = "#";
-	    }
+														  /* Set our params */
+														  /* BK: No need to use escaping when using parameters, in fact, you must not,
+														   * because you'll get literal '\' characters in your content. */
+														/*  $twitch = $_POST['twitch'] ?: '';*/
 
-			echo "<p>Youtube channel set to: ". $youtube ."</p>";
+														  /* Execute the prepared Statement */
+														  $status = $stmt->execute();
+														  /* BK: always check whether the execute() succeeded */
+														  if ($status === false) {
+														    trigger_error($stmt->error, E_USER_ERROR);
+														  }
+														  printf("<p>Playstation Network %d Row inserted.</p>\n", $stmt->affected_rows);
+														}
 
-		  $stmt = $mysqli->prepare("UPDATE users SET youtube=? WHERE id=?");
-		  /* BK: always check whether the prepare() succeeded */
-		  if ($stmt === false) {
-		    trigger_error($mysqli->error, E_USER_ERROR);
-		    return;
-		  }
-		  /* Bind our params */
-		  /* BK: variables must be bound in the same order as the params in your SQL.
-		   * Some people prefer PDO because it supports named parameter. */
-		  $stmt->bind_param('si', $discord, $id);
+														/* Check if POST contains Youtube channel */
+														if(isset($_POST['youtube']))
+														{
 
-		  /* Set our params */
-		  /* BK: No need to use escaping when using parameters, in fact, you must not,
-		   * because you'll get literal '\' characters in your content. */
-		/*  $twitch = $_POST['twitch'] ?: '';*/
+															if(substr(trim($_POST["youtube"]), 0, 23) == "https://www.youtube.com"){
+													        $youtube = trim($_POST["youtube"]);
+													    } else if (substr(trim($_POST["youtube"]), 0, 22) == "http://www.youtube.com")
+													      {
+													        $youtube = trim($_POST["youtube"]);
+													    } else if (substr(trim($_POST["youtube"]), 0, 19) == "https://youtube.com")
+													      {
+													        $youtube = trim($_POST["youtube"]);
+													    } else if (substr(trim($_POST["youtube"]), 0, 18) == "http://youtube.com")
+													      {
+													        $youtube = trim($_POST["youtube"]);
+													    } else {
+													        $youtube = "#";
+													    }
 
-		  /* Execute the prepared Statement */
-		  $status = $stmt->execute();
-		  /* BK: always check whether the execute() succeeded */
-		  if ($status === false) {
-		    trigger_error($stmt->error, E_USER_ERROR);
-		  }
-		  printf("<p>Youtube %d Row inserted.</p>\n", $stmt->affected_rows);
-		}
+															echo "<p>Youtube channel set to: ". $youtube ."</p>";
 
-		/* Check if POST contains Email address */
-		if(isset($_POST['email']))
-		{
-			$email = $_POST['email'];
-			echo "<p>Email address set to: ". $email ."</p>";
+														  $stmt = $mysqli->prepare("UPDATE users SET youtube=? WHERE id=?");
+														  /* BK: always check whether the prepare() succeeded */
+														  if ($stmt === false) {
+														    trigger_error($mysqli->error, E_USER_ERROR);
+														    return;
+														  }
+														  /* Bind our params */
+														  /* BK: variables must be bound in the same order as the params in your SQL.
+														   * Some people prefer PDO because it supports named parameter. */
+														  $stmt->bind_param('si', $discord, $id);
 
-		  $stmt = $mysqli->prepare("UPDATE users SET email=? WHERE id=?");
-		  /* BK: always check whether the prepare() succeeded */
-		  if ($stmt === false) {
-		    trigger_error($mysqli->error, E_USER_ERROR);
-		    return;
-		  }
-		  /* Bind our params */
-		  /* BK: variables must be bound in the same order as the params in your SQL.
-		   * Some people prefer PDO because it supports named parameter. */
-		  $stmt->bind_param('si', $discord, $id);
+														  /* Set our params */
+														  /* BK: No need to use escaping when using parameters, in fact, you must not,
+														   * because you'll get literal '\' characters in your content. */
+														/*  $twitch = $_POST['twitch'] ?: '';*/
 
-		  /* Set our params */
-		  /* BK: No need to use escaping when using parameters, in fact, you must not,
-		   * because you'll get literal '\' characters in your content. */
-		/*  $twitch = $_POST['twitch'] ?: '';*/
+														  /* Execute the prepared Statement */
+														  $status = $stmt->execute();
+														  /* BK: always check whether the execute() succeeded */
+														  if ($status === false) {
+														    trigger_error($stmt->error, E_USER_ERROR);
+														  }
+														  printf("<p>Youtube %d Row inserted.</p>\n", $stmt->affected_rows);
+														}
 
-		  /* Execute the prepared Statement */
-		  $status = $stmt->execute();
-		  /* BK: always check whether the execute() succeeded */
-		  if ($status === false) {
-		    trigger_error($stmt->error, E_USER_ERROR);
-		  }
-		  printf("<p>Email %d Row inserted.</p>\n", $stmt->affected_rows);
-		}
+														/* Check if POST contains Email address */
+														if(isset($_POST['email']))
+														{
+															$email = $_POST['email'];
+															echo "<p>Email address set to: ". $email ."</p>";
 
-		/* Check if POST contains twitter url */
-		if(isset($_POST['twitter']))
-		{
-			$twitter = $_POST['twitter'] ?: '';
-		  	echo "<p>Twitter account set to: ". $twitter ."</p>";
+														  $stmt = $mysqli->prepare("UPDATE users SET email=? WHERE id=?");
+														  /* BK: always check whether the prepare() succeeded */
+														  if ($stmt === false) {
+														    trigger_error($mysqli->error, E_USER_ERROR);
+														    return;
+														  }
+														  /* Bind our params */
+														  /* BK: variables must be bound in the same order as the params in your SQL.
+														   * Some people prefer PDO because it supports named parameter. */
+														  $stmt->bind_param('si', $discord, $id);
 
-		  $stmt2 = $mysqli2->prepare("UPDATE users SET twitter=? WHERE id=?");
-		  /* BK: always check whether the prepare() succeeded */
-		  if ($stmt2 === false) {
-		    trigger_error($mysqli2->error, E_USER_ERROR);
-		    return;
-		  }
-		  /*$id = 1;*/
-		  /* Bind our params */
-		  /* BK: variables must be bound in the same order as the params in your SQL.
-		   * Some people prefer PDO because it supports named parameter. */
-		  $stmt2->bind_param('si', $twitter, $id);
+														  /* Set our params */
+														  /* BK: No need to use escaping when using parameters, in fact, you must not,
+														   * because you'll get literal '\' characters in your content. */
+														/*  $twitch = $_POST['twitch'] ?: '';*/
 
-		  /* Set our params */
-		  /* BK: No need to use escaping when using parameters, in fact, you must not,
-		   * because you'll get literal '\' characters in your content. */
-		/*  $twitter = $_POST['twitter'] ?: '';*/
+														  /* Execute the prepared Statement */
+														  $status = $stmt->execute();
+														  /* BK: always check whether the execute() succeeded */
+														  if ($status === false) {
+														    trigger_error($stmt->error, E_USER_ERROR);
+														  }
+														  printf("<p>Email %d Row inserted.</p>\n", $stmt->affected_rows);
+														}
+
+														/* Check if POST contains twitter url */
+														if(isset($_POST['twitter']))
+														{
+															$twitter = $_POST['twitter'] ?: '';
+														  	echo "<p>Twitter account set to: ". $twitter ."</p>";
+
+														  $stmt2 = $mysqli2->prepare("UPDATE users SET twitter=? WHERE id=?");
+														  /* BK: always check whether the prepare() succeeded */
+														  if ($stmt2 === false) {
+														    trigger_error($mysqli2->error, E_USER_ERROR);
+														    return;
+														  }
+														  /*$id = 1;*/
+														  /* Bind our params */
+														  /* BK: variables must be bound in the same order as the params in your SQL.
+														   * Some people prefer PDO because it supports named parameter. */
+														  $stmt2->bind_param('si', $twitter, $id);
+
+														  /* Set our params */
+														  /* BK: No need to use escaping when using parameters, in fact, you must not,
+														   * because you'll get literal '\' characters in your content. */
+														/*  $twitter = $_POST['twitter'] ?: '';*/
 
 
-		  /* Execute the prepared Statement */
-		  $status2 = $stmt2->execute();
-		  /* BK: always check whether the execute() succeeded */
-		  if ($status2 === false) {
-		    trigger_error($stmt2->error, E_USER_ERROR);
-		  }
-		  printf("<p>Twitter %d Row inserted.</p>\n", $stmt2->affected_rows);
-		} else echo "No Post";
-	} else{
-		echo "Wrong password. Redirecting you back in 5 seconds..";
-		header( "refresh:5;url=social.php" );
+														  /* Execute the prepared Statement */
+														  $status2 = $stmt2->execute();
+														  /* BK: always check whether the execute() succeeded */
+														  if ($status2 === false) {
+														    trigger_error($stmt2->error, E_USER_ERROR);
+														  }
+														  printf("<p>Twitter %d Row inserted.</p>\n", $stmt2->affected_rows);
+														} else echo "No Post";
+													}
+
+											} else{
+													// Display an error message if password is not valid
+													$password_err = "The password you entered was not valid.";
+											}
+									}
+							}
+					} else{
+							echo "Oops! Something went wrong. Please try again later.";
+					}
+			}
+
+			// Close statement
+			mysqli_stmt_close($stmt);
 	}
+
+	// Close connection
+	mysqli_close($mysqli);
 }
 ?>
