d3e4a8f3ed985c9851cf947e27973e6549bb1d7e
zc4r <ekkert@myrkur.net>
2019-04-18 08:37:44 +0000

    s

--- gate/index.php
+++ gate/index.php
@@ -26,20 +26,20 @@ include '../medlimir/res/ip.php';
   </nav>
 
   <main>
-    <a href='https://player.fm/series/grinland'><h2>Grínland</a></h2><br/>
-    <a href='https://snorribjorns.libsyn.com/'><h2>The Snorri Björns Podcast Show</a></h2><br/>
-    <a href='https://player.fm/series/filalag'><h2>Fílalag</a></h2><br/>
-    <a href='https://www.podparadise.com/Podcast/1300530702'><h2>Markmannshanskarnir hans Alberts Camus</a></h2><br/>
-    <a href='https://www.podbean.com/podcast-detail/qnr4h-68197/%C3%86vint%C3%BDri-Tinna-Podcast'><h2>Ævintýri Tinna</a></h2><br/>
-    <a href='https://soundcloud.com/user-145982845'><h2>Helgaspjallið</a></h2><br/>
-    <a href='https://soundcloud.com/hj-rvar-hafli-ason'><h2>Dr. Football Podcast</a></h2><br/>
-    <a href='https://fotbolti.net/radio.php'><h2>Fótbolti.net</a></h2><br/>
-    <a href='http://www.kop.is/gamalt/podcast/'><h2>Gullkastið</a></h2><br/>
-    <a href='https://soundcloud.com/kjarninn/sets/hismid'><h2>Hismið</a></h2><br/>
-    <a href='https://soundcloud.com/kjarninn/sets/kvikan'><h2>Kvikan</a></h2><br/>
-    <a href='https://soundcloud.com/kjarninn/sets/ukl'><h2>Þukl</a></h2><br/>
-    <a href='https://soundcloud.com/kjarninn/sets/grettistak'><h2>Grettistak</a></h2><br/>
-    <a href='https://soundcloud.com/kjarninn/sets/taeknivarpi'><h2>Tæknivarpið</a></h2><br/>
+    <a href='https://player.fm/series/grinland'><h3>Grínland</a></h3>
+    <a href='https://snorribjorns.libsyn.com/'><h3>The Snorri Björns Podcast Show</a></h3>
+    <a href='https://player.fm/series/filalag'><h3>Fílalag</a></h3>
+    <a href='https://www.podparadise.com/Podcast/1300530703'><h3>Markmannshanskarnir hans Alberts Camus</a></h3>
+    <a href='https://www.podbean.com/podcast-detail/qnr4h-68197/%C3%86vint%C3%BDri-Tinna-Podcast'><h3>Ævintýri Tinna</a></h3>
+    <a href='https://soundcloud.com/user-145983845'><h3>Helgaspjallið</a></h3>
+    <a href='https://soundcloud.com/hj-rvar-hafli-ason'><h3>Dr. Football Podcast</a></h3>
+    <a href='https://fotbolti.net/radio.php'><h3>Fótbolti.net</a></h3>
+    <a href='http://www.kop.is/gamalt/podcast/'><h3>Gullkastið</a></h3>
+    <a href='https://soundcloud.com/kjarninn/sets/hismid'><h3>Hismið</a></h3>
+    <a href='https://soundcloud.com/kjarninn/sets/kvikan'><h3>Kvikan</a></h3>
+    <a href='https://soundcloud.com/kjarninn/sets/ukl'><h3>Þukl</a></h3><br/>
+    <a href='https://soundcloud.com/kjarninn/sets/grettistak'><h3>Grettistak</a></h3>
+    <a href='https://soundcloud.com/kjarninn/sets/taeknivarpi'><h3>Tæknivarpið</a></h3>
   </main>
 
   <aside>
