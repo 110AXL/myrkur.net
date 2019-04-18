2f31114147f973eb8937f8437fcaf242f66ae04d
110AXL <110AXL@myrkur.net>
2019-04-18 15:22:01 +0000

    a

--- medlimir/index.php
+++ medlimir/index.php
@@ -61,7 +61,7 @@ if(isset($nafn) === FALSE){
 				$removed_spaces = str_replace(" ","%20",$resized_link);
 
 				if(!$row["album"] == NULL)
-					echo "<a target=_blank title='" . $row["artist"]. " - " . $row["album"]. "' href='" . $row["link1"]. "'><img width=200 height=200 onError='this.onerror=null;this.src=/res/noid.jpg' src=/uploads/" . $removed_spaces . " /></a>";
+					echo "<a target=_blank title='" . $row["artist"]. " - " . $row["album"]. "' href='" . $row["link1"]. "'><img width=200 height=200 src=/uploads/" . $removed_spaces . " onError='this.onerror=null;this.src=/res/noid.jpg' /></a>";
 			}
 		echo "</body>";
 		}
