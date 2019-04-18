203c4ccf98bb7754625df68fd5c07cc2973c112f
zc4r <ekkert@myrkur.net>
2019-04-18 05:45:09 +0000

    a

--- config.php
+++ config.php
@@ -1,3 +1,4 @@
+<?php
 /*** This config is located in the root folder and goes on every page ***/
 
 // Start session
@@ -16,3 +17,4 @@ HeaderProcessor::initApp();
 HeaderProcessor::eventListener($_POST);
 // Use this function to help load up classes
 spl_autoload_register('autoLoader');
+?>
