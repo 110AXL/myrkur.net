ed1d2b7a7749da3439451bc6c572b29c4ae36db8
zc4r <ekkert@myrkur.net>
2019-04-18 11:00:05 +0000

    cs

--- login_success.php
+++ login_success.php
@@ -43,6 +43,24 @@ $email = isset($data['email']) ? $data['email']['address'] : '';
   <title>Account Kit PHP App</title>
 </head>
 <body>
+  <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '316701355598726',
+      xfbml      : true,
+      version    : 'v3.2'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
   <div>User ID: <?php echo $user_id; ?></div>
   <div>Phone Number: <?php echo $phone; ?></div>
   <div>Email: <?php echo $email; ?></div>
