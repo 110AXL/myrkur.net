06db39db1388c64835eb5318e9d643758a39708c
zc4r <ekkert@myrkur.net>
2019-04-18 08:34:37 +0000

    ss

--- gate/index.php
+++ gate/index.php
@@ -5,15 +5,15 @@
 
 <html lang="en">
 <head>
-<link rel="stylesheet" type="text/css" href="../res/default.css">
+<link rel="stylesheet" type="text/css" href="../medlimir/res/default.css">
 
-<?php include('../res/facebookPixelCode.php'); ?>
-<link rel="shortcut icon" type="image/png" href="../main/favicon-16x16.png"/>
+<?php include('../medlimir/res/facebookPixelCode.php'); ?>
+<link rel="shortcut icon" type="image/png" href="../medlimir/res/favicon-16x16.png"/>
 </head>
 <body bgcolor="black">
 <?php
 session_start();
-include '../res/ip.php';
+include '../medlimir/res/ip.php';
 ?>
 <div class="container">
   <header>
@@ -26,20 +26,20 @@ include '../res/ip.php';
   </nav>
 
   <main>
-    <a href='https://player.fm/series/grinland'>Grínland</a>
-    <a href='https://snorribjorns.libsyn.com/'>The Snorri Björns Podcast Show</a>
-    <a href='https://player.fm/series/filalag'>Fílalag</a>
-    <a href='https://www.podparadise.com/Podcast/1300530702'>Markmannshanskarnir hans Alberts Camus</a>
-    <a href='https://www.podbean.com/podcast-detail/qnr4h-68197/%C3%86vint%C3%BDri-Tinna-Podcast'>Ævintýri Tinna</a>
-    <a href='https://soundcloud.com/user-145982845'>Helgaspjallið</a>
-    <a href='https://soundcloud.com/hj-rvar-hafli-ason'>Dr. Football Podcast</a>
-    <a href='https://fotbolti.net/radio.php'>Fótbolti.net</a>
-    <a href='http://www.kop.is/gamalt/podcast/'>Gullkastið</a>
-    <a href='https://soundcloud.com/kjarninn/sets/hismid'>Hismið</a>
-    <a href='https://soundcloud.com/kjarninn/sets/kvikan'>Kvikan</a>
-    <a href='https://soundcloud.com/kjarninn/sets/ukl'>Þukl</a>
-    <a href='https://soundcloud.com/kjarninn/sets/grettistak'>Grettistak</a>
-    <a href='https://soundcloud.com/kjarninn/sets/taeknivarpi'>Tæknivarpið</a>
+    <a href='https://player.fm/series/grinland'><h2>Grínland</a></h2><br/>
+    <a href='https://snorribjorns.libsyn.com/'><h2>The Snorri Björns Podcast Show</a></h2><br/>
+    <a href='https://player.fm/series/filalag'><h2>Fílalag</a></h2><br/>
+    <a href='https://www.podparadise.com/Podcast/1300530702'><h2>Markmannshanskarnir hans Alberts Camus</a></h2><br/>
+    <a href='https://www.podbean.com/podcast-detail/qnr4h-68197/%C3%86vint%C3%BDri-Tinna-Podcast'><h2>Ævintýri Tinna</a></h2><br/>
+    <a href='https://soundcloud.com/user-145982845'><h2>Helgaspjallið</a></h2><br/>
+    <a href='https://soundcloud.com/hj-rvar-hafli-ason'><h2>Dr. Football Podcast</a></h2><br/>
+    <a href='https://fotbolti.net/radio.php'><h2>Fótbolti.net</a></h2><br/>
+    <a href='http://www.kop.is/gamalt/podcast/'><h2>Gullkastið</a></h2><br/>
+    <a href='https://soundcloud.com/kjarninn/sets/hismid'><h2>Hismið</a></h2><br/>
+    <a href='https://soundcloud.com/kjarninn/sets/kvikan'><h2>Kvikan</a></h2><br/>
+    <a href='https://soundcloud.com/kjarninn/sets/ukl'><h2>Þukl</a></h2><br/>
+    <a href='https://soundcloud.com/kjarninn/sets/grettistak'><h2>Grettistak</a></h2><br/>
+    <a href='https://soundcloud.com/kjarninn/sets/taeknivarpi'><h2>Tæknivarpið</a></h2><br/>
   </main>
 
   <aside>
