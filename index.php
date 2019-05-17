0ef5eda201b5c0d9ed4267669408170f35765c91
110AXL <110AXL@myrkur.net>
2019-05-17 11:21:43 +0000

    vendor
    vendor

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< current
 <?php
 // add in the config file
 require(__DIR__."/medlimir/config.php");
@@ -289,31 +288,3 @@ if(!empty($_SESSION["loggedin"]))
 </body>
 
 </html>
-=======
- <!-- Example -->
-<!DOCTYPE html>
-
-
-
-<html lang="en">
-<head>
-<link rel="stylesheet" type="text/css" href="./res/default.css">
-
-<?php include('./res/facebookPixelCode.php'); ?>
-<link rel="shortcut icon" type="image/png" href="main/favicon-16x16.png"/>
-</head>
-<body bgcolor="black">
- <?php
-session_start();
-include './res/ipgrabber.php';
-include './res/sqlcon.php';
-include './main/header.php';
-include './res/logo.php';
-echo "<div id='P'>";
-    include './main/box.php';
-echo "</div>";
-echo "Thanks, man.";
-?>
-</body>
-</html>
->>>>>>> before discard
