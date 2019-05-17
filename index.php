04b56421ebf215e6319ca4bdf812e6e5d5f1a9dd
110AXL <110AXL@myrkur.net>
2019-05-17 14:09:46 +0000

    galore

--- index.php
+++ index.php
@@ -77,12 +77,12 @@ if(!empty($_SESSION["loggedin"]))
   <div class="container h-100">
       <div class="row h-100 align-items-center justify-content-center text-center">
         <div class="col-lg-10 align-self-end">
-          <h1 class="text-uppercase text-white font-weight-bold">New music!</h1>
+          <h1 class="text-uppercase text-white font-weight-bold">New music album galore!</h1>
           <hr class="divider my-4">
         </div>
         <div class="col-lg-8 align-self-baseline">
-            <p class="text-white-75 font-weight-light mb-5">Myrkur.net strives to be the top quality music directory of the Internet!</p>
-            <a class="btn btn-primary btn-xl js-scroll-trigger" href="https://members.myrkur.net/addalbum.php">Add your own album!</a>
+            <p class="text-white-75 font-weight-light mb-5">Quality music for the everyday listener</p>
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="https://members.myrkur.net/addalbum.php">Add an album</a>
         </div>
       </div>
   </div>
