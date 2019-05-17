3a80d737ca1daa5f5a458149012137fcbc378969
110AXL <110AXL@myrkur.net>
2019-05-17 13:02:20 +0000

    sql

--- index.php
+++ index.php
@@ -37,6 +37,15 @@ if(!empty($_SESSION["loggedin"]))
 
 <body id="page-top">
 
+  <!-- php includes -->
+  <?php
+    include(__DIR__.'/medlimir/res/ip.php');
+    require_once 'main/sqlcon.php';
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+  ?>
+
   <!-- Navigation -->
   <nav id="mainNav" class="navbar navbar-expand-lg navbar-light fixed-top py-3">
     <div class="container">
@@ -138,6 +147,47 @@ if(!empty($_SESSION["loggedin"]))
   <!-- Portfolio Section -->
 <section id="portfolio">
   <div class="container-fluid p-0">
+    <div class='grid-container grid-container--fill'>
+    <?php
+
+      switch($_SERVER['QUERY_STRING']) {
+          case 'user':
+              $sql = "SELECT albums.id, albums.clicks, albums.artist, albums.year, albums.created_at, albums.album, albums.link1, albums.img, users.username FROM albums LEFT JOIN users ON albums.user_id = users.id ORDER BY users.username DESC";
+              break;
+          case 'artist':
+              $sql = "SELECT albums.id, albums.clicks, albums.artist, albums.year, albums.created_at, albums.album, albums.link1, albums.img, users.username FROM albums LEFT JOIN users ON albums.user_id = users.id ORDER BY albums.artist DESC";
+              break;
+          case 'added':
+              $sql = "SELECT albums.id, albums.clicks, albums.artist, albums.year, albums.created_at, albums.album, albums.link1, albums.img, users.username FROM albums LEFT JOIN users ON albums.user_id = users.id ORDER BY albums.created_at DESC";
+              break;
+          case 'released':
+              $sql = "SELECT albums.id, albums.clicks, albums.artist, albums.year, albums.created_at, albums.album, albums.link1, albums.img, users.username FROM albums LEFT JOIN users ON albums.user_id = users.id ORDER BY albums.year DESC";
+              break;
+          case 'clicks':
+              $sql = "SELECT albums.id, albums.clicks, albums.artist, albums.year, albums.created_at, albums.album, albums.link1, albums.img, users.username FROM albums LEFT JOIN users ON albums.user_id = users.id ORDER BY albums.clicks DESC";
+              break;
+
+          default:
+              $sql = "SELECT albums.id, albums.clicks, albums.artist, albums.year, albums.created_at, albums.album, albums.link1, albums.img, users.username FROM albums LEFT JOIN users ON albums.user_id = users.id ORDER BY albums.id DESC";
+      }
+
+      $result = $mysqli->query($sql);
+
+      if ($result->num_rows > 0) {
+       // output data of each row
+       while($row = $result->fetch_assoc()) {
+         $resized_link = $row["img"];
+         $removed_spaces = str_replace(" ","%20",$resized_link);
+         echo "<div class='grid-element'><a target=_blank title='" . $row["artist"]. " - " . $row["album"]. " [" . $row["username"] . "]' href=main/link.php?i='" . $row["id"]."'>";
+         echo "<img width=200 height=200 src=/medlimir/uploads/" . $removed_spaces . " />";
+         echo "<div class='title'><p>" . $row["artist"] . " - " . $row["album"] . "</a></p></div>";
+         echo "<p class='info' title='Year & link clicks.'><a href=main/link.php?i='" . $row["id"]."'>" . $row["year"] . "</a></p><div id=username>" . $row["username"] . "</div><div id=clicks>" . $row["clicks"] . "</div></div>";
+         }
+       }
+
+      $mysqli->close();?>
+    </div>
+    <!--
     <div class="row no-gutters">
         <div class="col-lg-4 col-sm-6">
             <a class="portfolio-box" href="img/portfolio/fullsize/1.jpg" >
@@ -228,7 +278,7 @@ if(!empty($_SESSION["loggedin"]))
           </a>
         </div>
     </div>
-
+ -->
   </div>
 
 </section>
