bf16f2328e4699fc6dc3867cdd45808515f02f76
110AXL <110AXL@myrkur.net>
2019-06-07 08:12:14 +0000

    s

--- index.php
+++ index.php
@@ -302,15 +302,16 @@ if(!empty($_SESSION["loggedin"]))
   <div class="container">
       <div class="row justify-content-center">
         <div class="col-lg-8 text-center">
-            <h2 class="mt-0">Let's Get In Touch</h2>
+            <h2 class="mt-0">About</h2>
             <hr class="divider my-4">
-            <p class="text-muted mb-5">Need more music?</p>
+            <p class="text-muted mb-5">Contact information & links</p>
         </div>
       </div>
       <div class="row">
         <div class="col-lg-4 ml-auto text-center">
-          <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
-          <div> +354 894-5585</div>
+          <i class="fas fa-bank fa-3x mb-3 text-muted"></i>
+          <a class="d-block" href="https://www.slsknet.org/news/">Soulseek</a>
+          <a class="d-block" href="http://ninjamovie.io">ninjamovie.io</a>
         </div>
         <div class="col-lg-4 mr-auto text-center">
           <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
