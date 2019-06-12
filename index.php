a3c61db48607277088e5cfb5bcfad19f656ae1d9
110AXL <110AXL@myrkur.net>
2019-06-12 23:29:26 +0000

    33

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ if(!empty($_SESSION["loggedin"]))
   <div class="container h-100">
       <div class="row h-100 align-items-center justify-content-center text-center">
         <div class="col-lg-10 align-self-end">
-          <h1 class="text-uppercase text-white font-weight-bold" style="top:33px;">m</h1>
+          <h1 class="text-uppercase text-white font-weight-bold">m</h1>
           <hr class="divider my-4">
         </div>
         <div class="col-lg-8 align-self-baseline">
