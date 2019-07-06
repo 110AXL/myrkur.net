d218c87c01e665cec64190abc48fa6d3b3f8b9cf
110AXL <45087880+110AXL@users.noreply.github.com>
2019-07-06 10:38:03 +0000

    Basic

---
+++ baukur/Index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <link rel="stylesheet" href="HolyGrail.css">
+  <title>My First HTML</title>
+  <meta charset="UTF-8">
+</head>
+<body>
+
+<div class="grid">
+  <header>
+    Header
+  </header>
+
+  <aside class="sidebar-left">
+    Left Sidebar
+  </aside>
+
+  <article>
+    Article
+  </article>
+
+  <aside class="sidebar-right">
+    Right Sidebar
+  </aside>
+  
+  <footer>
+    Footer
+  </footer>
+</div>
+</body>
+</html>
