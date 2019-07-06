a43682a0d54c1e6453eb4e9dafdccae80f066bd4
110AXL <45087880+110AXL@users.noreply.github.com>
2019-07-06 11:13:16 +0000

    Java

--- baukur/index.php
+++ baukur/index.php
@@ -29,5 +29,24 @@
     Skrá styrk 1000 | 1500 | 2000
   </footer>
 </div>
+
+<script>
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+</script>
+
+
 </body>
 </html>
