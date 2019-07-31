a6f6e2e1f66325739bccd128c70f43c21254d9d5
110AXL <benediktivars@gmail.com>
2019-07-31 02:52:37 +0000

    css

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ if(!empty($_SESSION["loggedin"]))
         <div class="link"><a class="nav-link js-scroll-trigger" href="http://wiki.myrkur.net">Wiki</a></div>
         <div class="link"><a class="nav-link js-scroll-trigger" href="https://members.myrkur.net/addalbum.php">Add album</a></div>
       </div>
-      <div id="logo">
+      <div id="logo" class="sticky">
           <a href="http://www.myrkur.net"><img src="oldie/img/myrkur-logo.jpg" /></a>
     </div>
   	</div>
